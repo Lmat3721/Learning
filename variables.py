1135a44ec194b0d8eafda1e1d489e4ae8a9ba910
Brett Dale <brett.dale@dcsg.com>
2023-01-13 16:17:43 -0500

    went over data types and variables pogchamp

---
+++ variables.py
@@ -0,0 +1,81 @@
+#variable assignment
+variable = 0
+
+#data types
+#string variable
+#primitive data types
+#strings, int, float, boolean
+
+#string variable
+variable = ""
+brett = "brett"
+last_name = "dale"
+
+#int
+num = 0
+num = 2458473463
+
+#float
+num = 0.0
+num = 50.55555
+
+#boolean
+variable = True
+variable = False
+
+#type casting - converting one data type to another
+
+#int to str
+variable = 0
+variable = str(variable)
+#variable is now "0"
+
+#int to float
+num = 0
+num = float(num)
+#print(str(num))
+#num is now 0.0
+
+#str to int
+number_as_string = "24"
+number_as_string = int(number_as_string)
+#number_as_string is now the integer 24
+
+#boolean notes
+#boolean can only be True or False
+variable = False
+
+num = "24"
+truth = (num == "20")
+truth = num is "24"
+print(truth)
+
+
+
+
+
+#concatenation - adding two or more strings together
+variable = "deez"
+description = "variable holds: " + variable
+print(description)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+num = 0
+#variable is being assigned
+truth = (num is not 0)
+print(truth)
+#same variable is getting a new value
+truth = (num is 0)
+print(truth)
