f152246d84641f08f4ba75b8de3e701c328cc956
Lmat3721 <122076086+Lmat3721@users.noreply.github.com>
2023-01-13 21:48:20 +0000

    Basic ass coding

---
+++ practice.py
@@ -0,0 +1,16 @@
+first = "lucas"
+last = "matthews"
+#print(f"{first} {last}")
+
+shit = 0
+shit = str(shit)
+#print(shit)
+
+#truth = "Who is going to win fantasy?"
+#print(truth)
+#truthh = "Lucas"
+#print(truthh)
+
+#temp = input("who is going to win fantasy: ")
+#print("winner is " + temp)
+
