ed4883737ca3334789671f0ca766a8c908678204
Lmat3721 <122076086+Lmat3721@users.noreply.github.com>
2023-01-20 21:54:52 +0000

    Second session practice

---
+++ practice2.py
@@ -0,0 +1,35 @@
+# import random
+# random_number = random.randint(1, 100)
+# print("random number: " + str(random_number))
+
+# if random_number < 50:
+#     print("It's less then 50 bozo")
+# elif random_number < 75:
+#     print("Fuck You")
+# elif random_number < 95:
+#     print("Mud up or Shut Up")
+# else:
+#     print("Congrats")
+
+items = []
+items.append(1)
+items.append(45)
+items.append(231)
+items.append(49)
+items.append(32)
+items.append(78)
+items.append(23)
+
+# counter = 0
+
+# length_of_list = len(items)
+# items.sort(reverse=True)
+
+# while (counter < length_of_list):
+#     item = str(items[counter])
+#     print("Item is: " + item)
+#     counter = counter + 1
+
+
+for number in items:
+    print(items)
